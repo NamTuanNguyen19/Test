b8f4894c9d8862c35811c000f54c83130fda6e96
NamTuanguyen19 <nguyentuannam19ptit@gmail.com>
2023-10-02 09:30:43 +0700

    9:30

---
+++ build/CMakeFiles/ChangeBri.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChangeBri.dir/ChangeBri.cpp.o"
+  "CMakeFiles/ChangeBri.dir/ChangeBri.cpp.o.d"
+  "ChangeBri"
+  "ChangeBri.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChangeBri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ChangeBri.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ChangeBri.
+# This may be replaced when dependencies are built.
